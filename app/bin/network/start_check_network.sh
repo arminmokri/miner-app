f2bfae0df5240e58b5ee61a1c7515d0ed9e4fbac
arminmokri <arminmokri@gmail.com>
2021-01-09 15:46:38 +0330

    change network logs

--- app/bin/network/start_check_network.sh
+++ app/bin/network/start_check_network.sh
@@ -16,6 +16,7 @@ ping_path2="$app_dir_path/bin/network/ping.sh '$network_url2'"
 send_reboot_cmd_to_modem_path="$app_dir_path/bin/network/send_cmd_to_modem.sh '$modem_ip' '$modem_username' '$modem_pasword' '$modem_reboot_cmd'"
 
 ### variables
+string=""
 ping_counter=0
 network_dis=0
 
@@ -27,18 +28,17 @@ do
    if [ "$ping_res1" -eq "0" ] || [ "$ping_res2" -eq "0" ] ### Internet Access
    then
       ###
-      if [ "$ping_counter" -gt "0" ]
+      if [ "$network_dis" -eq "1" ] || [ "$ping_counter" -gt "0" ]
       then
+         ###
          let ping_counter=ping_counter+1
-         echo -n " to $ping_counter"
-         ping_counter=0
-      fi
-      ###
-      if [ "$network_dis" -eq "1" ]
-      then
+         string="$string to $ping_counter"
+         echo $string
+         ###
          datetime_res=$(eval $datetime_path)
-         echo
          echo "$datetime_res | Network | Internet Access | Successed"
+         ###
+         ping_counter=0
          network_dis=0
       fi
    else ### No Internet Access
@@ -47,20 +47,23 @@ do
       ###
       if [ "$ping_counter" -eq "1" ] ### ping_counter = 1
       then
+         ###
          datetime_res=$(eval $datetime_path)
-         echo -n "$datetime_res | Network | Internet Access | Failed from 1 "
-      elif [ "$ping_counter" -gt "1" ] &&  [ "$ping_counter" -lt "50" ] ### 1 < ping_counter < 50
+         string="$datetime_res | Network | Internet Access | Failed from 1 "
+      elif [ "$ping_counter" -gt "1" ] && [ "$ping_counter" -lt "50" ] ### 1 < ping_counter < 50
       then
-         echo -n "."
+         ###
+         string="$string."
       elif [ "$ping_counter" -eq "50" ] ### ping_counter = 50
       then
          ###
-         echo -n " to 50"
+         string="$string to 50"
+         echo $string
          ###
          eval $send_reboot_cmd_to_modem_path
          datetime_res=$(eval $datetime_path)
-         echo
          echo "$datetime_res | Network | Reboot Modem | Successed"
+         ###
          ping_counter=0
          sleep 120
       fi

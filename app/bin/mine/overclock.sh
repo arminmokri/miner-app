8173f5589e55c13e1070fa0bc8aa315c12d1e288
arminmokri <arminmokri@gmail.com>
2021-03-26 01:44:52 +0430

    add overclock yes/no in config

--- app/bin/mine/overclock.sh
+++ app/bin/mine/overclock.sh
@@ -6,8 +6,11 @@ this_dir_path=$(eval "dirname $this_file_path")
 source "$this_dir_path/../../../config/main.conf"
 
 ### overclock
-for output in $(ls /sys/class/drm/ | egrep 'card[0-9]$')
-do
-   echo $pp_sclk_od | tee /sys/class/drm/$output/device/pp_sclk_od 1> /dev/null
-   echo $pp_mclk_od | tee /sys/class/drm/$output/device/pp_mclk_od 1> /dev/null
-done
+if [ "$overclock" == "yes" ]
+then
+   for output in $(ls /sys/class/drm/ | egrep 'card[0-9]$')
+   do
+      echo $pp_sclk_od | tee /sys/class/drm/$output/device/pp_sclk_od 1> /dev/null
+      echo $pp_mclk_od | tee /sys/class/drm/$output/device/pp_mclk_od 1> /dev/null
+   done
+fi
